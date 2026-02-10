f0c1a2ea88d68747b4a8f87bb140c2c08b1878b3
Goodonic <orexovheketa@gmail.com>
2026-02-10 19:59:47 +0700

    update: unified_interface.c: clean code

--- src/algorithms/core/unified_interface.c
+++ src/algorithms/core/unified_interface.c
@@ -1,10 +1,13 @@
-/**
- * ЗАДАЧА 4.1: Единый интерфейс библиотеки
- */
+/*!
+\file
+\brief Единый интерфейс библиотеки
+
+Данный файл содержит в себе определения основных
+классов, используемых в демонстрационной программе
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <stdbool.h>
 
 typedef enum
 {
@@ -20,7 +23,7 @@ typedef enum
     ALGO_PSEUDO_LEXICOGRAPHIC
 } permutation_algorithm_t;
 
-/**
+/*!
  * Коды ошибок
  */
 typedef enum
@@ -33,27 +36,6 @@ typedef enum
     PERM_ERROR_EMPTY_ARRAY = -5
 } permutation_error_t;
 
-static bool is_valid_array(int arr[], int n)
-{
-    if (!arr)
-        return false;
-
-    // Проверка на уникальность элементов (опционально, зависит от требований)
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                // Дубликаты допустимы в некоторых случаях
-                // Если нужна строгая проверка, можно вернуть false
-                fprintf(stderr, "Warning: Duplicate elements found at positions %d and %d\n", i, j);
-            }
-        }
-    }
-
-    return true;
-}
 static int* copy_array(const int arr[], int n)
 {
     if (!arr || n <= 0)
@@ -70,37 +52,32 @@ static int* copy_array(const int arr[], int n)
 
     return copy;
 }
-/**
+
+/*!
  * Основная функция библиотеки
- * @param algo Идентификатор алгоритма из enum permutation_algorithm_t
- * @param arr Массив элементов для перестановки
- * @param n Количество элементов в массиве
- * @param callback Функция обратного вызова, вызываемая для каждой перестановки
- * @return Код ошибки (0 в случае успеха)
- */
+ \param[in] algo Идентификатор алгоритма из enum permutation_algorithm_t
+ \param[in] arr Массив элементов для перестановкики
+ \param[in] n Количество элементов в массиве
+ \param[in] callback Функция обратного вызова, вызываемая для каждой перестановки
+ \param[in] callback Функция обратного вызова, вызываемая для каждой перестановки
+ \return Код ошибки (0 в случае успеха)
+*/
 int generatePermutations(permutation_algorithm_t algo, int arr[], int n,
                           void (*callback)(int perm[], int n))
 {
-    // Валидация входных параметров
-    // switch-case по значению algo
-    // Вызов соответствующей функции алгоритма
-    // Обработка ошибок (возврат кода ошибки или assert)
 
-    // Проверка указателя на массив
     if (!arr)
     {
         fprintf(stderr, "Error: Null array pointer\n");
         return PERM_ERROR_INVALID_INPUT;
     }
 
-    // Проверка размера массива
     if (n <= 0)
     {
         fprintf(stderr, "Error: Invalid array size: %d\n", n);
         return PERM_ERROR_INVALID_INPUT;
     }
 
-    // Проверка callback функции
     if (!callback)
     {
         fprintf(stderr, "Error: Null callback function\n");
@@ -118,44 +95,12 @@ int generatePermutations(permutation_algorithm_t algo, int arr[], int n,
 
     switch (algo)
     {
-        case ALGO_BACKTRACK:
-            backtrack_permute(arr_copy, n, 0, callback);
-        break;
-
-        case ALGO_RECURSIVE_SWAP:
-            recursive_swap_permute(arr_copy, n, 0, callback);
-        break;
-
         case ALGO_NARAYANA:
-            narayana_permute(arr_copy, n, callback);
+            permutations_narayana(arr_copy, n, callback);
         break;
 
         case ALGO_FACTORIAL_SYSTEM:
-            factorial_system_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_JOHNSON_TROTTER:
-            johnson_trotter_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_HEAP:
-            heap_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_MINIMAL_CHANGE:
-            minimal_change_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_CYCLE_LEADER:
-            cycle_leader_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_BINARY_MASKS:
-            binary_masks_permute(arr_copy, n, callback);
-        break;
-
-        case ALGO_PSEUDO_LEXICOGRAPHIC:
-            pseudo_lexicographic_permute(arr_copy, n, callback);
+            permutations_factorial_system(arr_copy, n, callback);
         break;
 
         default:
